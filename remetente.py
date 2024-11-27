4415452938392953fbf0dcf5297f803123159b39
Bernardoc14 <147110226+Bernardoc14@users.noreply.github.com>
2024-11-27 15:39:46 -0300

    Update remetente.py
    remetente agora envia checksum
    
    coloquei um break pra quando o pacote for enviado corretamente, aí ele para de enviar

--- remetente.py
+++ remetente.py
@@ -1,5 +1,8 @@
 import socket
 import time
+import json  
+
+
 
 # ISSO É MAQUINA C
 # A MÁQUINA C É O REMETENTE, ela deve ser capaz de enviar pacotes para a máquina B e aguardar o ACK
@@ -10,16 +13,18 @@ SERVER_PORT = 8080       # Porta da máquina intermediária
 TIMEOUT = 2              # Tempo limite para ACK (segundos)
 
 def remetente():
-    mensagem = "Hello, RDT 3.0!"
+    mensagem = "plutão não é mais planeta!!"
     seq_num = 0
+    checksum = calculate_checksum(f"{seq_num}:{mensagem}")
+
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.settimeout(TIMEOUT)
 
         while True:
             # Envia mensagem com número de sequência
-            pacote = f"{seq_num}:{mensagem}"
-            sock.sendto(pacote.encode(), (SERVER_IP, SERVER_PORT))
+            pacote = json.dumps({'sequencia': seq_num, 'mensagem': mensagem, 'checksum': checksum})
+            sock.sendto(pacote.encode(), (SERVER_IP, SERVER_PORT))  # Envia o pacote como bytes
             print(f"Enviado: {pacote}")
 
             try:
@@ -31,10 +36,15 @@ def remetente():
                     print(f"ACK {ack} recebido com sucesso!")
                     seq_num += 1
                     time.sleep(1)
+                    break
                 else:
                     print("ACK errado, retransmitindo...")
             except socket.timeout:
                 print("Timeout! Retransmitindo...")
+
+
+def calculate_checksum(data):
+    return sum(data.encode()) % 256
 
 if __name__ == "__main__":
     remetente()
