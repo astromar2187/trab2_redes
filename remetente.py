b7a01293a65cd2171e1c6571b159cae9c65c7459
Bernardoc14 <147110226+Bernardoc14@users.noreply.github.com>
2024-11-27 22:06:06 -0300

    Update remetente.py
    ele reenvia a mensagem ate receber o ack do destinatario

--- remetente.py
+++ remetente.py
@@ -2,24 +2,20 @@ import socket
 import time
 import json  
 
-
-
-# ISSO É MAQUINA C
+# ISSO É MÁQUINA C
 # A MÁQUINA C É O REMETENTE, ela deve ser capaz de enviar pacotes para a máquina B e aguardar o ACK
-# Segue protocolo pare e espere (stop-and-wait)
 
 SERVER_IP = '127.0.0.1'  # IP da máquina intermediária
 SERVER_PORT = 8080       # Porta da máquina intermediária
-TIMEOUT = 2              # Tempo limite para ACK (segundos)
+TIMEOUT = 2             # Tempo limite para ACK (segundos)
 
 def remetente():
-    mensagem = "plutão não é mais planeta!!"
+    mensagem = "plutao nao e mais planeta!!"
     seq_num = 0
     checksum = calculate_checksum(f"{seq_num}:{mensagem}")
 
-
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.settimeout(TIMEOUT)
+    
 
         while True:
             # Envia mensagem com número de sequência
@@ -27,21 +23,20 @@ def remetente():
             sock.sendto(pacote.encode(), (SERVER_IP, SERVER_PORT))  # Envia o pacote como bytes
             print(f"Enviado: {pacote}")
 
-            try:
-                # Aguarda ACK
-                resposta, _ = sock.recvfrom(1024)
-                ack = int(resposta.decode())
+            # Aguarda ACK
+            resposta, _ = sock.recvfrom(1024)
+            resposta = json.loads(resposta.decode())  # Decodifica a resposta recebida
+            if resposta.get('atraso'):
+                print("ACK atrasado, reenviando...")
+                continue
 
-                if ack == seq_num:
-                    print(f"ACK {ack} recebido com sucesso!")
-                    seq_num += 1
-                    time.sleep(1)
-                    break
-                else:
-                    print("ACK errado, retransmitindo...")
-            except socket.timeout:
-                print("Timeout! Retransmitindo...")
-
+            ack = resposta['sequencia']
+            if ack == seq_num:
+                print(f"ACK {ack} recebido com sucesso!")
+                seq_num += 1  # Atualiza o número de sequência para o próximo pacote
+                time.sleep(1)  # Pausa de 1 segundo entre envios (opcional)
+                break
+     
 
 def calculate_checksum(data):
     return sum(data.encode()) % 256
